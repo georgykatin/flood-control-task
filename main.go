6ff2e76e2b42ab101bf7dba8ce85f186830366a8
Georgy <96377078+georgykatin@users.noreply.github.com>
2024-03-20 13:48:41 +0300

    Add files via upload

--- main.go
+++ main.go
@@ -2,10 +2,36 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"task/floodcontroller"
+	"time"
 )
 
 func main() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379", // Замените на ваш адрес Redis
+		Password: "",               // Если пароль не пустой, укажите его здесь
+		DB:       0,                // По умолчанию используется DB 0
+	})
 
+	var flood FloodControl
+	flood = floodcontroller.NewFloodConfig(client, 20, 10*time.Second)
+
+	// Проверка алгоритма на 10 запросах
+	for i := 0; i < 10; i++ {
+		allowed, err := flood.Check(context.Background(), 222)
+		if err != nil {
+			log.Fatal(err)
+			return
+		}
+		if allowed {
+			fmt.Println("Request allowed")
+		} else {
+			fmt.Println("Request denied for")
+		}
+	}
 }
 
 // FloodControl интерфейс, который нужно реализовать.
